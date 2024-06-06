d19e3d6c9ce7b83c6ce6b859442542c42fbeafbc
PuerNya <safarier@outlook.com>
2024-06-06 22:17:55 +0800

    support config typed yaml/json5/hjson
    Co-authored-by: PuerNya <safarier@outlook.com>
    Signed-off-by: Salman Wahib <sxlmnwb@nodex.one>

--- experimental/libbox/config.go
+++ experimental/libbox/config.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/sagernet/sing-box"
 	"github.com/sagernet/sing-box/adapter"
+	"github.com/sagernet/sing-box/common/betterjson"
 	"github.com/sagernet/sing-box/common/process"
 	"github.com/sagernet/sing-box/experimental/libbox/platform"
 	"github.com/sagernet/sing-box/option"
@@ -20,7 +21,11 @@ import (
 )
 
 func parseConfig(configContent string) (option.Options, error) {
-	options, err := json.UnmarshalExtended[option.Options]([]byte(configContent))
+	parsedContent, err := betterjson.PreConvert([]byte(configContent))
+	if err != nil {
+		return option.Options{}, E.Cause(err, "decode config")
+	}
+	options, err := json.UnmarshalExtended[option.Options](parsedContent)
 	if err != nil {
 		return option.Options{}, E.Cause(err, "decode config")
 	}
